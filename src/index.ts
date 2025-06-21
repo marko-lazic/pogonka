7099a6e85fedc1996b5106f7b964edb337be869a
Marko Lazić <zila015@gmail.com>
2025-06-21 14:47:02 +0200

    Add handlebars

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import express, { Request, Response } from 'express';
+import { engine } from 'express-handlebars';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Configure handlebars
+app.engine('hbs', engine({
+  extname: '.hbs',
+  defaultLayout: 'layout',
+  layoutsDir: path.join(__dirname, '../views/layouts')
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../views'));
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.render('index', { 
+    message: 'Hello World!',
+    title: 'HWpogonka'
+  });
 });
 
 app.listen(port, () => {
