cd5e4628a0518185b242f7a945b4f80ad927010c
Marko Lazić <zila015@gmail.com>
2025-06-21 14:28:22 +0200

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Happy developing ✨')
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port} ✨`);
+});
